5208a3b8a05eba82b38243445f2fd507d3311a52
Joseph William Hauger <joseph>
2019-06-25 13:15:21 -0500

    working on my amazon!

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-//stuff
+
+    let productDivs = "";
+    for(let i=0; i < products.length; i++){
+        let product = products[i]; 
+        productDivs += `<div>${product.name}</div>`
+    }
+    document.getElementById("stuff").innerHTML = productDivs;
+
+    
+
+
+    
+    
