767e1f26bb5cc0dc02cc0853cf3ae264b9e8613f
Joseph William Hauger <joseph>
2019-06-26 19:35:04 -0500

    search bar done

--- public/index.js
+++ public/index.js
@@ -1,12 +1,22 @@
 
+    function Products(products){
     let productDivs = "";
     for(let i=0; i < products.length; i++){
         let product = products[i]; 
         productDivs += `<div>${product.name}</div>`
     }
     document.getElementById("stuff").innerHTML = productDivs;
+}
+Products(products);
 
-    
+function search(){
+    let searchWord = document.getElementById("searchBar").value;
+    let filteredProducts = products.filter(p=>p.name === searchWord)
+    ///p.name.indexOf or includes();
+    Products(filteredProducts);
+}
+
+
 
 
     
