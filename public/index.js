8ed82c7ebcb81e6b597ad8a131d243fba516d3bf
Joseph William Hauger <joseph>
2019-07-01 19:38:25 -0500

    don't stop now!

--- public/index.js
+++ public/index.js
@@ -1,15 +1,34 @@
 
-    function Products(products){
+   let shoppingCart = []:
+
+   function Products(products){
     let productDivs = [];
     for(let i=0; i < products.length; i++){
         let btn = document.createElement("button");
         let product = products[i]; 
-        productDivs += `<div>${product.name}</div>`;
-        btn.appendChild(product);
+        productDivs += `<div>${product.name}<button onclick="ProductDetail(${product.id})">View</button></div>`
     }
     document.getElementById("stuff").innerHTML = productDivs;
 }
+
+window.onload = ()=>{
 Products(products);
+}
+
+function ProductDetail(id){
+    let prod = prodcuts.find((p) =>{
+        return p.id ===id;
+    });
+
+
+    document.getElementById("productDetail").innerHTML = `
+    <div>${prod.name}</div>
+    <div>${prod.description}</div>
+    `;
+    //this gets rid of the other items when you click on more info for one.
+    document.getElementById("stuff").innerHTML = ""; 
+}
+//sessionStorage and localStorage <---use for shopping cart code!
 
 function search(){
     let searchWord = document.getElementById("searchBar").value;
