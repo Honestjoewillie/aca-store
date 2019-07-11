01848eb3eb2bb3d61a46843e7689f438ad2830f7
Joseph William Hauger <joseph>
2019-07-11 15:49:50 -0500

    push

--- public/index.js
+++ public/index.js
@@ -1,41 +1,71 @@
 
-   let shoppingCart = []:
+   let shoppingCart = [];
 
    function Products(products){
-    let productDivs = [];
+    let productDivs = "";
     for(let i=0; i < products.length; i++){
-        let btn = document.createElement("button");
         let product = products[i]; 
-        productDivs += `<div>${product.name}<button onclick="ProductDetail(${product.id})">View</button></div>`
+    productDivs += 
+    `<div>
+    <img src="${product.imgUrl}">
+    <h3>${product.name}</h3>
+    <div>${product.rating}&#9733 rating!</div>
+    <div>Description: ${product.description}</dov>
+    <div>There are ${product.reviews.length} reviews.</div>
+    <p>Price: ${product.price}</p>
+    <p><button id="infoButton" onclick='ProductDetail(${product.id})'>More info</button></p>
+    <p><button onclick='addCart(${product.id})'>Add to cart</button></p>
+    </div>`
     }
+    
     document.getElementById("stuff").innerHTML = productDivs;
 }
 
 window.onload = ()=>{
 Products(products);
 }
-
 function ProductDetail(id){
-    let prod = prodcuts.find((p) =>{
-        return p.id ===id;
-    });
-
-
-    document.getElementById("productDetail").innerHTML = `
-    <div>${prod.name}</div>
-    <div>${prod.description}</div>
-    `;
-    //this gets rid of the other items when you click on more info for one.
-    document.getElementById("stuff").innerHTML = ""; 
+    let moreInfo = products.filter(p=>p.id ===id)
+    Products(moreInfo);
+    //hide the button
+    document.getElementById('infoButton').style.display = "none";
+    //show all reviews and rating per review
+    //let fullReview = products.filter(p=>p.review ===review)
+    function showAll(reviews){
+    for(let i=0; i < products.reviews.length; i++){
+        let all = "";
+        let review = reviews[i];
+        all +=
+        console.log(review);
+    `<div id="infoFull">${review.rating}${review.description}</div>`;
+    }
+     const li = document.createElement('li');
+     li.appendChild(all);
 }
-//sessionStorage and localStorage <---use for shopping cart code!
-
+    
+}
 function search(){
     let searchWord = document.getElementById("searchBar").value;
     let filteredProducts = products.filter(p=>p.name === searchWord)
     ///p.name.indexOf or includes();
     Products(filteredProducts);
 }
+//I want this to show ALL reviews on click
+function review(review){
+    let read = products.filter(p=>p.reviews.description === review)
+    Products(read);
+}
+//shoping cart
+function addCart(id){
+    let addItem = products.filter(product => product.id ===id)
+    shoppingCart.push(addItem);
+    console.log(shoppingCart);
+}
+
+
+
+
+
 
 
 
