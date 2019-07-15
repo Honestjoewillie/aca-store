3b25655f155f187ea23e1c9bd47bdda3233cbc37
Joseph William Hauger <joseph>
2019-07-15 15:48:25 -0500

    still working on this

--- public/index.js
+++ public/index.js
@@ -2,47 +2,44 @@
    let shoppingCart = [];
 
    function Products(products){
+    let allProducts = document.getElementById('stuff');
     let productDivs = "";
     for(let i=0; i < products.length; i++){
         let product = products[i]; 
     productDivs += 
-    `<div>
+    `<div style = "display:fex;" id="prodcuts">
     <img src="${product.imgUrl}">
     <h3>${product.name}</h3>
     <div>${product.rating}&#9733 rating!</div>
     <div>Description: ${product.description}</dov>
     <div>There are ${product.reviews.length} reviews.</div>
     <p>Price: ${product.price}</p>
-    <p><button id="infoButton" onclick='ProductDetail(${product.id})'>More info</button></p>
+    <p><button id="infoButton" onclick='showAll(${product.id})'>More info</button></p>
+    <div id="review${product.id}"></div>
     <p><button onclick='addCart(${product.id})'>Add to cart</button></p>
     </div>`
     }
-    
     document.getElementById("stuff").innerHTML = productDivs;
 }
 
 window.onload = ()=>{
 Products(products);
 }
-function ProductDetail(id){
-    let moreInfo = products.filter(p=>p.id ===id)
-    Products(moreInfo);
+// function ProductDetail(id){
+//     let moreInfo = products.filter(p=>p.id ===id)
+//     Products(moreInfo);
     //hide the button
-    document.getElementById('infoButton').style.display = "none";
+    //document.getElementById('infoButton').style.display = "none";
     //show all reviews and rating per review
     //let fullReview = products.filter(p=>p.review ===review)
-    function showAll(reviews){
-    for(let i=0; i < products.reviews.length; i++){
+    function showAll(reviewId){
         let all = "";
-        let review = reviews[i];
-        all +=
-        console.log(review);
-    `<div id="infoFull">${review.rating}${review.description}</div>`;
-    }
-     const li = document.createElement('li');
-     li.appendChild(all);
-}
-    
+        let review = products.filter(p=>p.id === reviewId)
+        Products(review);
+        let productReview =document.getElementById("review${product.id}");
+        review[0].reviews.map(review =>
+            all+= `<div id="infoFull"><div>${review.rating}</div><div>${review.description}</div></div>`)
+            productReview.innerHTML = all;
 }
 function search(){
     let searchWord = document.getElementById("searchBar").value;
